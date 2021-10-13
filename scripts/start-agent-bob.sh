fdac801cb49976d7873c42d2404d9166d19cce23
Andrea Giacobino <no.andrea@gmail.com>
2021-10-13 21:06:45 +0200

    fix: command sequence and node config for the demo didexchange

--- scripts/start-agent-bob.sh
+++ scripts/start-agent-bob.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo "Starting bob agent on port: 8090"
-go run ../cmd/elesto-agent/main.go start \
+# go run ../cmd/elesto-agent/main.go start \
+
+
+dlv debug ../cmd/elesto-agent/main.go -- start \
 	--api-host localhost:8090 \
-	--inbound-host http@localhost:8091,ws@localhost:8092 \
-	--inbound-host-external http@http://localhost:8091,ws@ws://localhost:8092 \
+	--inbound-host http@localhost:8091 \
+	--inbound-host-external http@http://localhost:8091 \
 	--webhook-url http://localhost:8082 \
 	--agent-default-label BobAgent \
 	--database-type leveldb \
