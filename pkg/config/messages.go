742296ea69ee71cfafaec44893065fa05a7762df
Andrea Giacobino <no.andrea@gmail.com>
2021-11-12 12:11:06 +0100

    feat: connect credentials and balances panels

--- pkg/config/messages.go
+++ pkg/config/messages.go
@@ -3,19 +3,46 @@ package config
 
 // MsgHub contains channels used by the components to send messages to each others
 type MsgHub struct {
-	Notification   chan string
-	AgentWalletIn  chan string
-	AgentWalletOut chan string
-	TokenWalletIn  chan string
-	TokenWalletOut chan string
+	Notification   chan AppMsg
+	AgentWalletIn  chan AppMsg
+	AgentWalletOut chan AppMsg
+	TokenWalletIn  chan AppMsg
+	TokenWalletOut chan AppMsg
 }
 
 func NewMsgHub() *MsgHub {
 	return &MsgHub{
-		Notification:   make(chan string, 4096),
-		AgentWalletIn:  make(chan string, 4096),
-		AgentWalletOut: make(chan string, 4096),
-		TokenWalletIn:  make(chan string, 4096),
-		TokenWalletOut: make(chan string, 4096),
+		Notification:   make(chan AppMsg, 4096),
+		AgentWalletIn:  make(chan AppMsg, 4096),
+		AgentWalletOut: make(chan AppMsg, 4096),
+		TokenWalletIn:  make(chan AppMsg, 4096),
+		TokenWalletOut: make(chan AppMsg, 4096),
+	}
+}
+
+const (
+	// MsgBalances from chain to ui with a list of update balances
+	MsgBalances = iota
+	// MsgDidDoc from chain to ui with a did document (at startup)
+	MsgDidDoc
+	// MsgChainOfTrust returns the list of verifiable credentials for a DENOM
+	MsgChainOfTrust
+	// MsgPublicVCs returns the list of verifiable credentials
+	MsgPublicVCs
+	// MsgPublicVCData returns the details of a verifiable credential
+	MsgPublicVCData
+
+)
+
+// AppMsg are messages that are exchanged within the app
+type AppMsg struct {
+	Typ int
+	Payload interface{}
+}
+
+func NewAppMsg(typ int, payload interface{}) AppMsg{
+	return AppMsg{
+		Typ:     typ,
+		Payload: payload,
 	}
 }
---
+++ pkg/wallets/chain/query.go
@@ -0,0 +1,143 @@
+package chain
+
+import (
+	"context"
+	didTypes "github.com/allinbits/cosmos-cash/v2/x/did/types"
+	vcTypes "github.com/allinbits/cosmos-cash/v2/x/verifiable-credential/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	log "github.com/sirupsen/logrus"
+)
+
+// Balance retrieves the "cash" balance for an account
+func (cc *ChainClient) Balance(address string) *sdk.Coin {
+	bankClient := banktypes.NewQueryClient(cc.ctx)
+	bankRes, err := bankClient.Balance(
+		context.Background(),
+		&banktypes.QueryBalanceRequest{Address: address, Denom: "cash"},
+	)
+	if err != nil {
+		log.Fatalln("error requesting balance", err)
+	}
+	log.Infoln("balance for", address, "is", bankRes.GetBalance())
+	return bankRes.GetBalance()
+}
+
+// Balances retrieves all the balances for an account
+func (cc *ChainClient) Balances(address string) sdk.Coins {
+	bankClient := banktypes.NewQueryClient(cc.ctx)
+	bankRes, err := bankClient.AllBalances(
+		context.Background(),
+		&banktypes.QueryAllBalancesRequest{Address: address},
+	)
+	if err != nil {
+		log.Fatalln("error requesting balance", err)
+	}
+	log.Infoln("balances for", address, "are", bankRes.GetBalances())
+	return bankRes.GetBalances()
+}
+
+// GetChainOfTrust retrieve the chain of trust for a token DENOM
+func (cc *ChainClient) GetChainOfTrust(denom string) (cot []vcTypes.VerifiableCredential) {
+	client := vcTypes.NewQueryClient(cc.ctx)
+	res, err := client.VerifiableCredentials(
+		context.Background(),
+		&vcTypes.QueryVerifiableCredentialsRequest{},
+	)
+	if err != nil {
+		log.Fatalln("error requesting balance", err)
+	}
+
+	// first search for the license
+	var issuerDID didTypes.DID
+	for _, v := range res.GetVcs() {
+		switch s := v.CredentialSubject.(type) {
+		case *vcTypes.VerifiableCredential_LicenseCred:
+			if s.LicenseCred.CirculationLimit.GetDenom() == denom {
+				cot = append(cot, v)
+				issuerDID = v.GetSubjectDID()
+				break
+			}
+		}
+	}
+	// if the license is not found then return
+	if issuerDID.String() == "" {
+		return
+	}
+
+	// search for the issuer registartion credential
+	var regulatorDID didTypes.DID
+	for _, v := range res.GetVcs() {
+		switch v.CredentialSubject.(type) {
+		case *vcTypes.VerifiableCredential_RegistrationCred:
+			if v.GetSubjectDID() == issuerDID {
+				cot = append(cot, v)
+				regulatorDID = v.GetIssuerDID()
+				break
+			}
+		}
+	}
+
+	if regulatorDID.String() == "" {
+		return
+	}
+
+	// search for the regulator credentials
+	for _, v := range res.GetVcs() {
+		switch v.CredentialSubject.(type) {
+		case *vcTypes.VerifiableCredential_RegulatorCred:
+			if v.GetSubjectDID() == regulatorDID {
+				cot = append(cot, v)
+				regulatorDID = v.GetIssuerDID()
+				break
+			}
+		}
+	}
+	return
+}
+
+// DIDDoc retrieve a did document for a
+func (cc *ChainClient) DIDDoc(didID string) didTypes.DidDocument {
+	client := didTypes.NewQueryClient(cc.ctx)
+	res, err := client.DidDocument(context.Background(), &didTypes.QueryDidDocumentRequest{Id: didID})
+	if err != nil {
+		log.Fatalln("error requesting balance", err)
+	}
+	log.Infoln("did document for", didID, "is", res.GetDidDocument())
+	return res.GetDidDocument()
+}
+
+// GetHolderPublicVCS retrieve the VCS holded by a did
+func (cc *ChainClient) GetHolderPublicVCS(didID string) (vcs []vcTypes.VerifiableCredential) {
+
+	client := vcTypes.NewQueryClient(cc.ctx)
+	res, err := client.VerifiableCredentials(
+		context.Background(),
+		&vcTypes.QueryVerifiableCredentialsRequest{},
+	)
+	if err != nil {
+		log.Fatalln("error requesting balance", err)
+	}
+	holderDID := didTypes.DID(didID)
+	for _, v := range res.GetVcs() {
+		if v.GetSubjectDID() == holderDID {
+			vcs = append(vcs, v)
+		}
+	}
+
+	return
+}
+
+// GetPublicVC retrieve a vc by id
+func (cc *ChainClient) GetPublicVC(vcID string) vcTypes.VerifiableCredential {
+
+	client := vcTypes.NewQueryClient(cc.ctx)
+	res, err := client.VerifiableCredential(
+		context.Background(),
+		&vcTypes.QueryVerifiableCredentialRequest{VerifiableCredentialId: vcID},
+	)
+	if err != nil {
+		log.Fatalln("error requesting balance", err)
+	}
+	return res.GetVerifiableCredential()
+}
