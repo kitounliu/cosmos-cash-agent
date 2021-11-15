4de1617c64c119468c867547626dc457fcab8f5c
PaddyMc <paddymchale@hotmail.com>
2021-11-15 05:28:31 +0000

    feat: ssi client set up

--- pkg/wallets/ssi/aries.go
+++ pkg/wallets/ssi/aries.go
@@ -1,31 +1,98 @@
 package ssi
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"time"
 
-	"github.com/hyperledger/aries-framework-go/component/storageutil/mem"
-
 	"github.com/allinbits/cosmos-cash-agent/pkg/config"
-	"github.com/hyperledger/aries-framework-go/pkg/didcomm/transport"
-	arieshttp "github.com/hyperledger/aries-framework-go/pkg/didcomm/transport/http"
-	"github.com/hyperledger/aries-framework-go/pkg/didcomm/transport/ws"
-	"github.com/hyperledger/aries-framework-go/pkg/vdr/httpbinding"
-	"github.com/hyperledger/aries-framework-go/pkg/wallet"
 
+	"github.com/hyperledger/aries-framework-go/component/storageutil/mem"
+	"github.com/hyperledger/aries-framework-go/pkg/client/didexchange"
+	"github.com/hyperledger/aries-framework-go/pkg/client/mediator"
+	"github.com/hyperledger/aries-framework-go/pkg/client/messaging"
+	de "github.com/hyperledger/aries-framework-go/pkg/controller/command/didexchange"
+	"github.com/hyperledger/aries-framework-go/pkg/didcomm/common/service"
+	"github.com/hyperledger/aries-framework-go/pkg/didcomm/messaging/msghandler"
+	"github.com/hyperledger/aries-framework-go/pkg/didcomm/transport"
+	"github.com/hyperledger/aries-framework-go/pkg/didcomm/transport/ws"
 	"github.com/hyperledger/aries-framework-go/pkg/framework/aries"
 	"github.com/hyperledger/aries-framework-go/pkg/framework/context"
+	"github.com/hyperledger/aries-framework-go/pkg/vdr/httpbinding"
+	"github.com/hyperledger/aries-framework-go/pkg/wallet"
 
 	log "github.com/sirupsen/logrus"
 )
 
 var (
-	w *wallet.Wallet
+	w      *wallet.Wallet
+	client = &http.Client{}
+	reqURL string
 )
 
 type SSIWallet struct {
-	w   *wallet.Wallet
-	ctx *context.Provider
+	w                 *wallet.Wallet
+	ctx               *context.Provider
+	didExchangeClient *didexchange.Client
+	routeClient       *mediator.Client
+	messagingClient   *messaging.Client
+}
+
+func createDIDExchangeClient(ctx *context.Provider) *didexchange.Client {
+	// create a new did exchange client
+	didExchange, err := didexchange.New(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	actions := make(chan service.DIDCommAction, 1)
+
+	err = didExchange.RegisterActionEvent(actions)
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		service.AutoExecuteActionEvent(actions)
+	}()
+
+	return didExchange
+}
+
+func createRoutingClient(ctx *context.Provider) *mediator.Client {
+	// create the mediator client this client handler routing between edge and cloud agents
+	routeClient, err := mediator.New(ctx)
+	if err != nil {
+		panic(err)
+	}
+	events := make(chan service.DIDCommAction)
+
+	err = routeClient.RegisterActionEvent(events)
+	if err != nil {
+		panic(err)
+	}
+	go func() {
+		service.AutoExecuteActionEvent(events)
+	}()
+
+	return routeClient
+}
+
+func createMessagingClient(ctx *context.Provider) *messaging.Client {
+	n := LocalNotifier{}
+	registrar := msghandler.NewRegistrar()
+
+	msgClient, err := messaging.New(ctx, registrar, n)
+	if err != nil {
+		panic(err)
+	}
+
+	return msgClient
+
 }
 
 func Agent(name, pass, resolverURL string) *SSIWallet {
@@ -33,10 +100,8 @@ func Agent(name, pass, resolverURL string) *SSIWallet {
 	provider := mem.NewProvider()
 	stateProvider := mem.NewProvider()
 
-	// ws inbound, outbound
+	// ws outbound
 	var transports []transport.OutboundTransport
-	outboundHTTP, err := arieshttp.NewOutbound(arieshttp.WithOutboundHTTPClient(&http.Client{}))
-	transports = append(transports, outboundHTTP)
 	outboundWs := ws.NewOutbound()
 	transports = append(transports, outboundWs)
 
@@ -54,7 +119,7 @@ func Agent(name, pass, resolverURL string) *SSIWallet {
 		aries.WithOutboundTransports(transports...),
 		aries.WithTransportReturnRoute("all"),
 		aries.WithVDR(httpVDR),
-		aries.WithVDR(CosmosVDR{}),
+	//	aries.WithVDR(CosmosVDR{}),
 	)
 	// get the context
 	ctx, err := framework.Context()
@@ -66,12 +131,42 @@ func Agent(name, pass, resolverURL string) *SSIWallet {
 	if err != nil {
 		panic(err)
 	}
+
 	// creating vcwallet instance for user with local KMS settings.
 	w, err = wallet.New(name, ctx)
 	if err != nil {
 		panic(err)
 	}
-	return &SSIWallet{w: w, ctx: ctx}
+
+	didExchangeClient := createDIDExchangeClient(ctx)
+	routeClient := createRoutingClient(ctx)
+	messagingClient := createMessagingClient(ctx)
+
+	return &SSIWallet{
+		w:                 w,
+		ctx:               ctx,
+		didExchangeClient: didExchangeClient,
+		routeClient:       routeClient,
+		messagingClient:   messagingClient,
+	}
+}
+
+func (cw *SSIWallet) HandleInvitation(
+	invitation *de.CreateInvitationResponse,
+) *didexchange.Connection {
+	connectionID, err := cw.didExchangeClient.HandleInvitation(invitation.Invitation)
+	if err != nil {
+		panic(err)
+	}
+
+	connection, err := cw.didExchangeClient.GetConnection(connectionID)
+	if err != nil {
+		panic(err)
+	}
+	log.Infoln("Connection created", connection)
+
+	return connection
+
 }
 
 // Run should be called as a goroutine, the parameters are:
@@ -99,7 +194,6 @@ func (cw *SSIWallet) Run(hub *config.MsgHub) {
 		}
 	}()
 
-
 	// here an example how to listen to internal messages
 	for {
 		m := <-hub.AgentWalletIn
@@ -114,8 +208,61 @@ func (cw *SSIWallet) Run(hub *config.MsgHub) {
 			// always send to the notification channel for the UI
 			// handle the notification in the ui/handlers.go dispatcher function
 			hub.Notification <- config.NewAppMsg(m.Typ, vc)
+		case config.MsgHandleInvitation:
+			log.Debugln(
+				"TokenWallet received MsgHandleInvitation msg for ",
+				m.Payload.(string),
+			)
+			var invite de.CreateInvitationResponse
+			reqURL = fmt.Sprint(
+				"http://localhost:8090",
+				"/connections/create-invitation?&label=BobMediatorEdgeAgent",
+			)
+			post(client, reqURL, nil, &invite)
+
+			// TODO: validate invitation is correct
+			connection := cw.HandleInvitation(&invite)
+
+			hub.Notification <- config.NewAppMsg(config.MsgHandleInvitation, connection)
+
 		}
 	}
+}
+
+// TODO remove in favor of public did exchange, here for test purposes
+func request(client *http.Client, method, url string, requestBody io.Reader, val interface{}) {
+	req, err := http.NewRequest(method, url, requestBody)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	json.Unmarshal(bodyBytes, &val)
+}
+
+func post(client *http.Client, url string, requestBody, val interface{}) {
+	if requestBody != nil {
+		request(client, "POST", url, bitify(requestBody), val)
+	} else {
+		request(client, "POST", url, nil, val)
+	}
+
+}
+func bitify(in interface{}) io.Reader {
+	v, err := json.Marshal(in)
+	if err != nil {
+		panic(err.Error())
+	}
+	return bytes.NewBuffer(v)
 }
 
 // AcceptContactRequest
